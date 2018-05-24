8e56d2e0869c96230430d757a12de8e23821c4da
huynhtuanhuy <huynhtuanhuy1996@gmail.com>
2018-05-24 16:44:17 +0700

    Up

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ let app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post("/webhook", (req, res)=>{
-    console.log("Some thing is comming!", JSON.stringify(req.headers));
+    console.log("Some thing is comming!", JSON.stringify(req.headers.masterkey));
     if(!req.body) res.status(400).send({ success: 0, msg: 'Body data is empty!' })
-    else if(!req.headers.masterKey || req.headers.masterKey != configs.masterKey) res.status(400).send({ success: 0, msg: 'Master key is wrong or missing!' })
+    else if(!req.headers.masterkey || req.headers.masterkey != configs.masterKey) res.status(400).send({ success: 0, msg: 'Master key is wrong or missing!' })
     else {
         console.log("Body receiver: " + JSON.stringify(req.body));
         // console.log()

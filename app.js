8b78eda7f08f8ab60f81fef368fc21afd0835ec6
huynhtuanhuy <huynhtuanhuy1996@gmail.com>
2018-05-25 18:35:34 +0700

    a

--- app.js
+++ app.js
@@ -16,21 +16,34 @@ app.post("/webhook", (req, res)=>{
     if(!req.body) res.status(400).send({ success: 0, msg: 'Body data is empty!' })
     else if(!req.headers.masterkey || req.headers.masterkey != configs.masterKey) res.status(400).send({ success: 0, msg: 'Master key is wrong or missing!' })
     else {
-        console.log("Body receiver: " + JSON.stringify(req.body.result));
-        if(req.body.result && req.body.result.parameters && req.body.result.parameters["currency-from"] && req.body.result.parameters["currency-to"]) {
-            currency.convertCurrency(req.body.result.parameters["currency-from"], req.body.result.parameters["currency-to"], req.body.result.parameters["amount"] || 1, (err, result)=>{
-                if(err) console.error(err)
-                else {
-                    console.log(`Right now, if you exchange ${util.numberFormat(req.body.result.parameters["amount"])}${req.body.result.parameters["currency-from"]} to ${req.body.result.parameters["currency-to"]}, you'll get ${util.numberFormat(result)}${req.body.result.parameters["currency-to"]}`)
+        console.log("Body receiver: " + req.body.action);
+        switch (req.body.action) {
+            case "currency.convert":
+                if(req.body.result && req.body.result.parameters && req.body.result.parameters["currency-from"] && req.body.result.parameters["currency-to"]) {
+                    currency.convertCurrency(req.body.result.parameters["currency-from"], req.body.result.parameters["currency-to"], req.body.result.parameters["amount"] || 1, (err, result)=>{
+                        if(err) console.error(err)
+                        else {
+                            console.log(`Right now, if you exchange ${util.numberFormat(req.body.result.parameters["amount"])}${req.body.result.parameters["currency-from"]} to ${req.body.result.parameters["currency-to"]}, you'll get ${util.numberFormat(result)}${req.body.result.parameters["currency-to"]}`)
+                            res.json({
+                                displayText: `Right now, if you exchange ${util.numberFormat(req.body.result.parameters["amount"])}${req.body.result.parameters["currency-from"]} to ${req.body.result.parameters["currency-to"]}, you'll get ${util.numberFormat(result)}${req.body.result.parameters["currency-to"]}`,
+                                // displayText: [{ text: { text: [`Right now, if you exchange ${util.numberFormat(req.body.result.parameters["amount"])}${req.body.result.parameters["currency-from"]} to ${req.body.result.parameters["currency-to"]}, you'll get ${util.numberFormat(result)}${req.body.result.parameters["currency-to"]}`] }}],
+                                source: "Penguin Webhook"
+                            });
+                        }
+                    });
+                } else {
                     res.json({
-                        displayText: `Right now, if you exchange ${util.numberFormat(req.body.result.parameters["amount"])}${req.body.result.parameters["currency-from"]} to ${req.body.result.parameters["currency-to"]}, you'll get ${util.numberFormat(result)}${req.body.result.parameters["currency-to"]}`,
-                        // displayText: [{ text: { text: [`Right now, if you exchange ${util.numberFormat(req.body.result.parameters["amount"])}${req.body.result.parameters["currency-from"]} to ${req.body.result.parameters["currency-to"]}, you'll get ${util.numberFormat(result)}${req.body.result.parameters["currency-to"]}`] }}],
-                        source: ""
+                        displayText: "Could you provide me the details?",
+                        source: "Penguin Webhook"
                     });
                 }
-            });
-        } else {
-            res.status(400).send({ success: 0, msg: 'Nothing to convert' });
+                break;
+            default:
+                res.json({
+                    displayText: "Sorry, something went wrong and i can't do this for you right now.",
+                    source: "Penguin Webhook"
+                });
+                break;
         }
     }
 });

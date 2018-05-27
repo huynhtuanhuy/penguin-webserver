e4ae3371b8cc0ad94348b05f4a7bbfd36546b4a2
huynhtuanhuy <huynhtuanhuy1996@gmail.com>
2018-05-27 20:16:22 +0700

    a

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.post("/webhook", (req, res)=>{
         let actions = req.body.result && req.body.result.action ? req.body.result.action.split(".") : "";
         switch (actions[0]) {
             case "currency":
+                console.log(JSON.stringify(req.body))
                 if(req.body.result && req.body.result.parameters && req.body.result.parameters["currency-from"] && req.body.result.parameters["currency-to"]) {
                     currency.convertCurrency(req.body.result.parameters["currency-from"], req.body.result.parameters["currency-to"], req.body.result.parameters["amount"] || 1, (err, result)=>{
                         if(err) console.error(err)

7f650d0a51d73c9f564238460bbf50cfafe45846
huynhtuanhuy <huynhtuanhuy1996@gmail.com>
2018-05-31 14:08:12 +0700

    Ahihi

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const configs = require('./configs.json');
-const currency = require('./currency_convert');
+const convert = require('./convert');
 const util = require('./util');
 
 let app = express();
@@ -14,26 +14,53 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.post("/webhook", (req, res)=>{
-    console.log(req.body)
     if(!req.body) res.status(400).send({ success: 0, msg: 'Body data is empty!' })
     else if(!req.headers.masterkey || req.headers.masterkey != configs.masterKey) res.status(400).send({ success: 0, msg: 'Master key is wrong or missing!' })
     else if(req.body.result && req.body.result.action) {
         let actions = req.body.result.action;
         if(actions == "currency.convert") {
             if(req.body.result && req.body.result.parameters && req.body.result.parameters["currency-from"] && req.body.result.parameters["currency-to"]) {
-                currency.convertCurrency(req.body.result.parameters["currency-from"], req.body.result.parameters["currency-to"], req.body.result.parameters["amount"] || 1, (err, result)=>{
+                convert.convertCurrency(req.body.result.parameters["currency-from"], req.body.result.parameters["currency-to"], req.body.result.parameters["amount"] || 1, (err, result)=>{
                     if(err) console.error(err)
                     else {
-                        let data = `Right now, if you exchange ${util.numberFormat(req.body.result.parameters["amount"])} ${req.body.result.parameters["currency-from"]} to ${req.body.result.parameters["currency-to"]}, you'll get ${util.numberFormat(result)} ${req.body.result.parameters["currency-to"]}`;
+                        let speech = `Right now, if you exchange ${util.numberFormat(req.body.result.parameters["amount"])} ${req.body.result.parameters["currency-from"]} to ${req.body.result.parameters["currency-to"]}, you'll get ${util.numberFormat(result)} ${req.body.result.parameters["currency-to"]}`;
                         res.send({
-                            messages: [{ type: 0, speech: data }],
+                            messages: [{ type: 0, speech: speech }],
                             source: "Penguin Webhook"
                         });
                     }
                 });
             } else {
                 res.send({
-                    messages: [{ type: 0, speech: "Could you provide me more details?" }],
+                    messages: [{ type: 0, speech: "Not enough parameter, please try again!" }],
+                    source: "Penguin Webhook"
+                });
+            }
+        } else if(actions == "units.convert") {
+            if(req.body.result && req.body.result.parameters && req.body.result.parameters["unit-from"] && req.body.result.parameters["unit-to"]) {
+                let result = convert.convertUnit(req.body.result.parameters["unit-from"], req.body.result.parameters["unit-to"], req.body.result.parameters["amount"]);
+                let speech = `${util.numberFormat(req.body.result.parameters["amount"])}${req.body.result.parameters["unit-from"]} equals ${util.numberFormat(result)}${req.body.result.parameters["currency-to"]}`;
+                res.send({
+                    messages: [{ type: 0, speech: speech }],
+                    source: "Penguin Webhook"
+                });
+            } else {
+                res.send({
+                    messages: [{ type: 0, speech: "Not enough parameter, please try again!" }],
+                    source: "Penguin Webhook"
+                });
+            }
+        } else if(actions == "units.convert") {
+            if(req.body.result && req.body.result.parameters && req.body.result.parameters["unit-from"] && req.body.result.parameters["unit-to"]) {
+                let result = convert.convertUnit(req.body.result.parameters["unit-from"], req.body.result.parameters["unit-to"], req.body.result.parameters["amount"]);
+                let speech = `${util.numberFormat(req.body.result.parameters["amount"])}${req.body.result.parameters["unit-from"]} equals ${util.numberFormat(result)}${req.body.result.parameters["currency-to"]}`;
+                res.send({
+                    messages: [{ type: 0, speech: speech }],
+                    source: "Penguin Webhook"
+                });
+            } else {
+                res.send({
+                    messages: [{ type: 0, speech: "Not enough parameter, please try again!" }],
                     source: "Penguin Webhook"
                 });
             }
--- convert.js
+++ convert.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
+const convertUnits = require('convert-units');
 
 const convertCurrency = (from, to, amount, cb) => {
-    let url = `https://free.currencyconverterapi.com/api/v5/convert?q=${from}_${to}&compact=y`
+    let url = `https://free.currencyconverterapi.com/api/v5/convert?q=${from}_${to}&compact=y`;
 
     axios.get(url)
         .then(function (response) {
@@ -14,6 +15,11 @@ const convertCurrency = (from, to, amount, cb) => {
         });
 }
 
+const convertUnit = (from, to, amount) => {
+    return convertUnits(amount).from(from).to(to);
+}
+
 module.exports = {
-    convertCurrency
+    convertCurrency,
+    convertUnit
 }
